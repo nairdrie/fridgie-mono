1459c8627d1055cd0d65334a305cc8b9a35b199e
Nick Airdrie <nick.airdrie@gmail.com>
2026-09-21 17:41:44 -0400

    readd share plugin

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -79,11 +79,8 @@ function MotionStack({ children }: { children: React.ReactNode }) {
 }
 
 export default function RootLayout() {
-  const nativeSharingDisabled = Platform.OS === 'web' ||
-    (Platform.OS === 'ios' && process.env.EXPO_PUBLIC_IOS_SHARE_EXTENSION_ENABLED === 'false');
-
   return (
-      <ShareIntentProvider options={{ scheme: 'fridgie', resetOnBackground: false, disabled: nativeSharingDisabled }}>
+      <ShareIntentProvider options={{ scheme: 'fridgie', resetOnBackground: false, disabled: Platform.OS === 'web' }}>
       <SharedRecipeProvider>
       <SafeAreaProvider>
         <GlassPreferencesProvider>
